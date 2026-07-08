9c75f759dbfdb7303989cae24a3ee83116514ac1
samuel-garmany <65299214+samuel-garmany@users.noreply.github.com>
2026-07-08 12:31:56 -0600

    fix syntax error

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
-      desktop = nixpkgs.lib.nixossystem {
+      desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 	modules = [
 	  ./desktop/configuration.nix
