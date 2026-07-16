1d480834aa99cad40edaf2ab22423c2a1be93aa5
samuel-garmany <65299214+samuel-garmany@users.noreply.github.com>
2026-07-16 00:25:13 -0600

    allow homemanager to configure bash

--- modules/shells/terminal.nix
+++ modules/shells/terminal.nix
@@ -1,5 +1,6 @@
 {
   config.hm.base = { ... }: {
+    programs.bash.enable = true;
     programs.zoxide.enable = true;
     programs.direnv.enable = true;
     programs.direnv.nix-direnv.enable = true;
