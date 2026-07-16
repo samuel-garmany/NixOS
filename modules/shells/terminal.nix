2179526f9c085bb437646c81a03cb54584a59032
samuel-garmany <git config --global user.email>
2026-07-16 00:34:55 -0600

    remove unused bash command from home manager

--- modules/shells/terminal.nix
+++ modules/shells/terminal.nix
@@ -1,6 +1,5 @@
 {
   config.hm.base = { ... }: {
-    programs.bash.enable = true;
     programs.zoxide.enable = true;
     programs.direnv.enable = true;
     programs.direnv.nix-direnv.enable = true;
