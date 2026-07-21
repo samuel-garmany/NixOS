d3e27fb3346d2699ccd1d91b3a37ad5cc074462d
samuel-garmany <65299214+samuel-garmany@users.noreply.github.com>
2026-07-21 14:53:42 -0600

    switch to faugus launcher

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -1,41 +1,37 @@
 {
-  config.nixos.base =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = with pkgs; [
-        arduino-ide
-        audacity
-        bitwarden-desktop
-        cine
-        freecad
-        freetube
-        gimp
-        hunspell
-        hunspellDicts.en_US
-        hyphenDicts.en_US
-        inkscape
-        jre8
-        libreoffice
-        ltex-ls
-        obs-studio
-        orca-slicer
-        poppler-utils
-        qgis
-        slack
-        texlive.combined.scheme-full
-        unzip
-        yubioath-flutter
-        zoom-us
-      ];
-    };
+  config.nixos.base = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+      arduino-ide
+      audacity
+      bitwarden-desktop
+      cine
+      freecad
+      freetube
+      gimp
+      hunspell
+      hunspellDicts.en_US
+      hyphenDicts.en_US
+      inkscape
+      jre8
+      libreoffice
+      ltex-ls
+      obs-studio
+      orca-slicer
+      poppler-utils
+      qgis
+      slack
+      texlive.combined.scheme-full
+      unzip
+      yubioath-flutter
+      zoom-us
+    ];
+  };
 
-  config.nixos.desktop =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = with pkgs; [
-        discord
-        lutris
-        prismlauncher
-      ];
-    };
+  config.nixos.desktop = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
+      discord
+      faugus-launcher
+      prismlauncher
+    ];
+  };
 }
